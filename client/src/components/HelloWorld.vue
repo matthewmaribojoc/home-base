d4720c89b422e91cf38dec4d2f6f1df10fad20a1
matthewmaribojoc <matthewmaribojoc@gmail.com>
2019-02-08 19:43:49 -0500

    initial commit

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template lang='html'>
+  <div>
+    {{homes}}
+  </div>
+</template>
+
+<script>
+import HomeAPI from '@/services/HomeAPI.js'
+
+export default {
+  data () {
+    return {
+      homes: []
+    }
+  },
+  mounted () {
+    this.loadHomes()
+  },
+  methods: {
+    async loadHomes () {
+      const response = await HomeAPI.getHomes()
+      this.homes = response.data
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='css'>
+
+</style>
